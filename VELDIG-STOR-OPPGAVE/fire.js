8a1b1038bc9406ab9846927685e6789725957ca5
domukas08 <domsin20@vgs.nfk.no>
2026-03-10 14:27:03 +0100

    legge hele filen inn
    har en kopi i dokumenter hvis dette ikke virker

---
+++ VELDIG-STOR-OPPGAVE/fire.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCdjcRfpU1fvPZsFr_7U03zrrJtmQO1erg",
+authDomain: "nettside-domas.firebaseapp.com",
+projectId: "nettside-domas",
+storageBucket: "nettside-domas.firebasestorage.app",
+messagingSenderId: "260489160705",
+appId: "1:260489160705:web:311fefc2e0ee8a42eb3800"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
